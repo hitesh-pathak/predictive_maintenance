ad2a40baf7136e1d36479a127c57bb71a397ce03
hitesh-pathak <97353760+hitesh-pathak@users.noreply.github.com>
2022-05-15 17:04:54 +0530

    created initial version of preprocessing, without clustering and scaling.

---
+++ code/data_preprocessing/preprocessing.py
@@ -0,0 +1,276 @@
+import os.path
+import pandas as pd
+import numpy as np
+import math
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
+from pandas.api.types import is_numeric_dtype
+# from imblearn.over_sampling import SMOTE
+
+class Preprocessor:
+    """
+        This class shall  be used to clean and transform the data before training.
+
+        Written By: iNeuron Intelligence
+        Version: 1.0
+        Revisions: None
+
+        """
+
+    def __init__(self, file_object, logger_object):
+        self.file_object = file_object
+        self.logger_object = logger_object
+
+    def remove_columns(self,data,columns):
+        """
+                Method Name: remove_columns
+
+                Description: This method removes the given columns from a pandas dataframe.
+                Output: A pandas DataFrame after removing the specified columns.
+
+                On Failure: Raise Exception
+        """
+        self.logger_object.log(self.file_object, 'Entered the remove_columns method of the Preprocessor class')
+        try:
+            useful_data = data.drop(labels=columns, axis=1) # drop the labels specified in the columns
+            self.logger_object.log(self.file_object,
+                                   'Column removal Successful. Exiting remove_columns method.')
+            return useful_data
+        except Exception as e:
+            self.logger_object.log(self.file_object, f'Exception occurred in removing columns: {e}')
+            self.logger_object.log(self.file_object, 'Exiting the remove_columns method of the Preprocessor class.')
+            raise e
+
+    def separate_label_feature(self, data, label_column_name):
+        """
+                Method Name: separate_label_feature
+
+                Description: This method separates the features and a Label Columns.
+                Output: Returns two separate Dataframes, one containing features and the other containing Labels.
+
+                On Failure: Raise Exception
+        """
+        self.logger_object.log(self.file_object, 'Entered the separate_label_feature method of the Preprocessor class')
+        try:
+            self.X=data.drop(labels=label_column_name,axis=1) # drop the columns specified and separate the feature columns
+            self.Y=data[label_column_name] # Filter the Label columns
+            self.logger_object.log(self.file_object,
+                                   'Label Separation Successful. Exited the separate_label_feature method of the Preprocessor class')
+            return self.X,self.Y
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occured in separate_label_feature method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'Label Separation Unsuccessful. Exited the separate_label_feature method of the Preprocessor class')
+            raise Exception()
+
+    def is_null_present(self,data):
+        """
+            Method Name: is_null_present
+
+            Description: This method checks whether there are null values present in the pandas Dataframe or not.
+            Output: Returns a Boolean Value. True if null values are present in the DataFrame,
+            False if they are not present.
+
+            On Failure: Raise Exception
+        """
+        self.logger_object.log(self.file_object, 'Entered the is_null_present method of the Preprocessor class')
+        try:
+            null_present = False
+            null_counts = data.isna().sum()  # check for the count of null values per column
+            for col in null_counts.index:
+                if null_counts[col] > 0:
+                    null_present=True
+                    self.logger_object.log(self.file_object, f"Null values present in {col} column.")
+                    break
+
+            self.logger_object.log(self.file_object, "Creating preprocessing data directory if not present.")
+            if not os.path.isdir('preprocessing_data'):
+                os.makedirs('preprocessing_data')
+
+            if null_present: # write the logs to see which columns have null values
+                self.logger_object.log(self.file_object, "Writing null value count to file.")
+
+                pd.DataFrame(null_counts, columns=['Missing value count']).to_csv('preprocessing_data/null_values.csv')
+
+                self.logger_object.log(self.file_object, "Written null value count to file null_values.csv")
+
+            self.logger_object.log(self.file_object,'Exiting the is_null_present method of the Preprocessor class')
+
+            return null_present
+        except Exception as e:
+            self.logger_object.log(self.file_object,f"Error: {e}")
+            self.logger_object.log(self.file_object,'Exiting the is_null_present method of the Preprocessor class')
+            raise e
+
+    def impute_missing_values(self, data):
+        """
+                    Method Name: impute_missing_values
+
+                    Description: This method replaces all the missing values in the Dataframe using KNN Imputer.
+                    Output: A Dataframe which has all the missing values imputed.
+
+                    On Failure: Raise Exception
+        """
+        self.logger_object.log(self.file_object,
+                               'Entered the impute_missing_values method of the Preprocessor class')
+        try:
+            imputer = KNNImputer(n_neighbors=3, weights='uniform',missing_values=np.nan)
+            new_array=imputer.fit_transform(data)  # impute the missing values
+            # convert the nd-array from the step above to a Dataframe
+            new_data=pd.DataFrame(data=new_array, columns=data.columns)
+            self.logger_object.log(self.file_object, 'Imputing missing values Successful.')
+            self.logger_object.log(self.file_object,
+                                   'Exited the impute_missing_values method of the Preprocessor class')
+            return new_data
+        except Exception as e:
+            self.logger_object.log(self.file_object, f'Exception occured in impute_missing_values method: {e}')
+            self.logger_object.log(self.file_object,
+                                   'Exiting the impute_missing_values method of the Preprocessor class ')
+            raise e
+
+    def drop_columns_with_zero_std_deviation(self,data):
+        """
+                        Method Name: get_columns_with_zero_std_deviation
+
+                        Description: This method finds out the columns which have a standard deviation of zero
+                        and drops them.
+
+                        On Failure: Raise Exception
+
+                        :param data: DataFrame object
+                        :return: cleaned DataFrame
+        """
+        self.logger_object.log(self.file_object,
+                               'Entered the drop_columns_with_zero_std_deviation method of the Preprocessor class')
+        try:
+            columns = data.columns
+            data_n = data.describe()
+            col_to_drop = []
+            for x in columns:
+                # self.logger_object.log(self.file_object, f"Checking column {x} for zero standard deviation.")
+                if math.isclose(data_n.loc['std', x], 0):  # check if standard deviation is zero
+                    self.logger_object.log(self.file_object, f"Column {x} needs to be dropped.")
+                    col_to_drop.append(x)  # prepare the list of columns with standard deviation zero
+
+            if not col_to_drop:
+                self.logger_object.log(self.file_object, 'No columns need to be dropped.')
+                return data
+
+            self.logger_object.log(self.file_object,
+                                   'Column search for Standard Deviation of Zero Successful. Dropping columns.')
+            return self.remove_columns(data, col_to_drop)
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,f'Error: {e}')
+            self.logger_object.log(self.file_object,
+                                   'Exiting the drop_columns_with_zero_std_deviation method of the Preprocessor class')
+            raise e
+
+    def drop_redundant_settings(self, data):
+        '''
+        Method Name: drop_redundant_settings
+
+        Description: This method checks if the setting names need to be dropped based on the data.
+        If so the columns are dropped and the cleaned dataframe is returned. On failure Exception is raised.
+
+        :param data: DataFrame object
+        :return: cleaned DataFrame
+        '''
+        self.logger_object.log(self.file_object,
+                               'Entered the get_redundant_settings method of the Preprocessor class')
+        try:
+            columns = data.columns
+            # it is necessary for grouper to be a list, a ndarray or pandas series will error out
+            setting_names = list(columns[2:5])  # this is according to schema file col 3, 4, 5 are settings
+            rounding = {cols: digits for cols, digits in zip(setting_names, [0, 2, 0])}  # specifies the rounding digit
+
+            # groupby automatically drops na values so na values do not affect this.
+            setting_no = data[setting_names].round(rounding).groupby(by=setting_names, dropna=True).size().shape[0]
+            self.logger_object.log(self.file_object, f"Found {setting_no} setting(s).")
+            if setting_no > 1:
+                self.logger_object.log(self.file_object, "No settings columns dropped.")
+                return data
+            else:
+                self.logger_object.log(self.file_object, "Dropping settings columns.")
+                return self.remove_columns(data, setting_names)
+
+        except Exception as e:
+            self.logger_object.log(self.file_object, f"Error occurred while dropping setting columns: {e}")
+            self.logger_object.log(self.file_object, f"Exiting drop_redundant_settings method of {__class__} class.")
+            raise e
+
+    def scaleData(self, data):
+        '''
+        Method Name: scaleData
+
+        Description: This method scales the numerical columns of the given dataframe and outputs the scaled dataframe.
+
+        On Failure: Raise Exception
+        :param data: DataFrame
+        :return: DataFrame with scaled numerical values
+        '''
+        self.logger_object.log(self.file_object, f"Starting scaleData method of {__class__} class.")
+        try:
+            scalar = StandardScaler()
+
+            self.logger_object.log(self.file_object, "Finding all numeric datatype columns.")
+            numcol = []
+            for cols in data.columns:
+                if  is_numeric_dtype(data.cols):
+                    numcol.append(cols)
+                else:
+                    self.logger_object.log(self.file_object, f"Column {cols} has a non numeric type")
+
+            num_data = data[numcol]
+            # non_num_data = [col for col in data.cols and col not in num_data]
+            scaled_array = scalar.fit_transform(num_data)
+
+            data[numcol] = num_data
+            scaled_data = pd.DataFrame(scaled_array, columns=numcol, index=data.index)
+
+            data[numcol] = scaled_data
+            # final_data = pd.concat([num_data, cat_data], axis=1)
+            self.logger_object.log(self.file_object,
+                                   f"Data scaled successfully. Exiting scaleData method of {__class__} class.")
+
+            return data
+
+        except Exception as e:
+            self.logger_object.log(self.file_object, f"Error occurred while scaling data: {e}")
+            self.logger_object.log(self.file_object,
+                                   f"Exiting scaleData method of {__class__} class.")
+            raise e
+
+    def add_remaining_useful_life(self, data):
+        '''
+        Method Name: add_remaining_useful_life
+
+        This methods adds the remaining useful life (RUL) column to the given dataframe.
+
+        On Failure: Raise Exception
+
+        :param data: DataFrame
+        :return: DataFrame with remaining useful life column
+        '''
+        self.logger_object.log(self.file_object, f"Starting add_remaining_useful_life method of {__class__} class.")
+        try:
+            # find max time cycle for each unit
+            group_unit = data.groupby('unit_nr')['time_cycles']
+
+            for unit, slice in group_unit:
+                rul = slice.max() - slice
+                data.loc[data['unit_nr'] == unit, 'RUL'] = rul
+
+            else:
+                self.logger_object.log(self.file_object, f"Successfully added remaining useful life column to data.")
+                return data
+
+        except Exception as e:
+            self.logger_object.log(self.file_object, f"Error occurred while adding RUL column: {e}")
+            self.logger_object.log(self.file_object,
+                                   f"Exiting add_remaining_useful_life method of {__class__} class.")
+            raise e
+
+
+
+
+
---
+++ code/trainingModel.py
@@ -0,0 +1,120 @@
+"""
+This is the Entry point for Training the Machine Learning Model.SSS
+"""
+# Doing the necessary imports
+import os
+
+from sklearn.model_selection import train_test_split
+from data_ingestion import data_loader
+from data_preprocessing import preprocessing
+# from data_preprocessing import clustering
+# from best_model_finder import tuner
+# from file_operations import file_methods
+from application_logging import logger
+
+#Creating the common Logging object
+
+
+class trainModel:
+
+    def __init__(self):
+        self.log_writer = logger.App_Logger()
+        if not os.path.isdir('Training_Logs'):
+            os.makedirs('Training_Logs')
+        self.file_object = open("Training_Logs/ModelTrainingLog.txt", 'a+')
+        self.log_writer.log(self.file_object, f"Created an instance of {__class__} class.")
+        self.file_object.close()
+
+    def trainingModel(self):
+        with open("Training_Logs/ModelTrainingLog.txt", 'a+') as self.file_object:
+            self.log_writer.log(self.file_object, 'Start of Training')
+            try:
+                # Getting the data from the source
+                self.log_writer.log(self.file_object, 'Start data ingestion.')
+                data_getter = data_loader.Data_Getter(self.file_object, self.log_writer)
+                datagen = data_getter.get_data()
+                self.log_writer.log(self.file_object, 'Data ingestion completed.')
+
+                for data, filename in datagen():
+                    """doing the data preprocessing"""
+                    self.log_writer.log(self.file_object, f"Loaded data from {filename}.")
+
+                    self.log_writer.log(self.file_object, "Initialize Preprocessor class.")
+                    preprocessor = preprocessing.Preprocessor(self.file_object,self.log_writer)
+
+                    self.log_writer.log(self.file_object, "Dropping columns with zero standard deviation.")
+                    data = preprocessor.drop_columns_with_zero_std_deviation(data)
+
+                    self.log_writer.log(self.file_object, "Dropping redundant setting columns.")
+                    data = preprocessor.drop_redundant_settings(data)
+
+                    # impute null values
+                    self.log_writer.log(self.file_object, "Checking data for null values.")
+                    if preprocessor.is_null_present(data):
+                        self.log_writer.log(self.file_object,
+                                            "Data contains columns with null values, imputing null values")
+
+                        data = preprocessor.impute_missing_values(data)
+                    else:
+                        self.log_writer.log(self.file_object, "No columns with null values found in data.")
+
+                    # add RUL column
+                    self.log_writer.log(self.file_object, "Adding remaining useful life column to data.")
+                    data = preprocessor.add_remaining_useful_life(data)
+
+                    print(data.shape, data.columns)
+                    print(data)
+                    print('.' * 20)
+
+            #
+
+                # X = data.drop(['class'], axis=1)
+                # Y = data['class']
+                #
+                # """ Applying the clustering approach"""
+                #
+            #     kmeans=clustering.KMeansClustering(self.file_object,self.log_writer)  # object initialization.
+            #     number_of_clusters=kmeans.elbow_plot(X)  # using the elbow plot to find the number of optimum clusters
+            #
+            #     # Divide the data into clusters
+            #     X = kmeans.create_clusters(X,number_of_clusters)
+            #
+            #     # create a new column in the dataset consisting of the corresponding cluster assignments.
+            #     X['Labels']=Y
+            #
+            #     # getting the unique clusters from our dataset
+            #     list_of_clusters=X['Cluster'].unique()
+            #
+            #     """parsing all the clusters and looking for the best ML algorithm to fit on individual cluster"""
+            #
+            #     for i in list_of_clusters:
+            #         cluster_data=X[X['Cluster']==i] # filter the data for one cluster
+            #
+            #         # Prepare the feature and Label columns
+            #         cluster_features=cluster_data.drop(['Labels','Cluster'],axis=1)
+            #         cluster_label= cluster_data['Labels']
+            #
+            #         # splitting the data into training and test set for each cluster one by one
+            #         x_train, x_test, y_train, y_test = train_test_split(cluster_features, cluster_label, test_size=1 / 3, random_state=355)
+            #         x_train = preprocessor.scaleData(x_train)
+            #         x_test = preprocessor.scaleData(x_test)
+            #
+            #         model_finder=tuner.Model_Finder(self.file_object,self.log_writer) # object initialization
+            #
+            #         # getting the best model for each of the clusters
+            #         best_model_name,best_model=model_finder.get_best_model(x_train,y_train,x_test,y_test)
+            #
+            #         # saving the best model to the directory.
+            #         file_op = file_methods.File_Operation(self.file_object,self.log_writer)
+            #         save_model=file_op.save_model(best_model,best_model_name+str(i))
+            #
+            # # logging the successful Training
+            # self.log_writer.log(self.file_object, 'Successful End of Training')
+            # self.file_object.close()
+
+            except Exception as e:
+                # logging the unsuccessful Training
+                self.log_writer.log(self.file_object, f"Error: {e}")
+                self.log_writer.log(self.file_object, 'Unsuccessful End of Training')
+                # self.file_object.close()
+                raise e
