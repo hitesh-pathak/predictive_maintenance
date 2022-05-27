d978c24db181f64183dab3da61ac737417a53048
hitesh-pathak <97353760+hitesh-pathak@users.noreply.github.com>
2022-05-27 15:13:41 +0530

    deploy using angular not complete

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from rq import Queue
+import json
 from rq.job import Job
 from worker import conn
 from gen_prediction import gen_prediction
@@ -11,31 +12,21 @@ app = Flask(__name__)
 # set up a redis queue based on the connection
 q = Queue(connection=conn, default_timeout = 1000)
 
-# Set up main route
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    results = {}
-    if request.method == 'POST':
-        # Extract the input
-        path = request.form.get('filepath')
+    return render_template('index.html')
 
-        # put the prediction job into redis queue
-        job = q.enqueue_call(func=gen_prediction, args=(path, ), result_ttl=1000,)
 
-        print(job.get_id())
+@app.route('/start', methods=['POST'])
+def get_counts():
 
-        return render_template("index.html", results=results)
+    # get url
+    data = json.loads(request.data.decode())
+    filepath = data["filepath"]
 
-    elif request.method == 'GET':
-        return render_template('index.html')
-
-@app.route('/results/<job_key>', methods=['GET'])
-def get_results(job_key):
-
-    job = Job.fetch(job_key, connection=conn)
-
-    if job.is_finished:
-        return str(job.result), 200
-
-    else:
-        return 'Not finished yet', 202
+    # start job
+    job = q.enqueue_call(
+        func=gen_prediction, args=(filepath,), result_ttl=1000
+    )
+    # return created job id
+    return job.get_id()
--- static/main.js
+++ static/main.js
@@ -1,15 +1,57 @@
-// this is only a test
 (function () {
   'use strict';
 
-  angular.module('WordcountApp', [])
+  angular.module('RULpredictor', [])
 
-  .controller('WordcountController', ['$scope', '$log',
-    function($scope, $log) {
+  .controller('RULpredictorController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {
+
+      $scope.submitButtonText = 'Submit';
+      $scope.loading = false;
+
       $scope.getResults = function() {
+
         $log.log("test");
+
+        // get filepath
+        var userInput = $scope.filepath;
+
+        // fire api request
+        $http.post('/start', {"filepath": userInput}).
+          success(function(results) {
+            $log.log(results);
+            getStatus(results);
+          }).
+          error(function(error) {
+            $log.log(error);
+          });
+
+    };
+
+    function getStatus(jobID) {
+      var timeout = "";
+
+      var poller = function() {
+        // fire polling request to get status
+        $http.get('/results/'+jobID).
+          success(function(data, status, headers, config) {
+            if (status === 202) {
+              $log.log(data, status);
+            } else if (status === 200) {
+                $log.log(data);
+                $scope.status = data;
+                $timeout.cancel(timeout);
+                return false;
+            }
+            // continue to call the poller() every 2 seconds
+            // until timeout is cancelled
+            timeout = $timeout(poller, 2000);
+          });
       };
+      poller ();
+
     }
-  ]);
+
+}]);
 
 }());
