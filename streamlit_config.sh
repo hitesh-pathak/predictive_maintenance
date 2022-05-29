36da449e819c3119c289f77a76bc8b7194905466
hitesh-pathak <97353760+hitesh-pathak@users.noreply.github.com>
2022-05-29 15:13:21 +0530

    streamlit config bugs

--- streamlit_config.sh
+++ streamlit_config.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 mkdir -p ~/.streamlit/
 
-echo "\
+PORT=${PORT:-8501}
+
+echo -e "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "streamlit config done!"
